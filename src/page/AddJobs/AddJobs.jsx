3f9715958a7b8c840755585f564b179c6a03b1f7
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-05 12:53:58 +0600

    add jobs from add

--- src/page/AddJobs/AddJobs.jsx
+++ src/page/AddJobs/AddJobs.jsx
@@ -1,189 +1,239 @@
 import { useContext, useState } from "react";
-import { AuthContext } from "../../provider/AuthProvider";
-// import Swal from "sweetalert2";
+import DatePicker from "react-datepicker";
 
-const AddJobs = () => {
+import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../../provider/AuthProvider";
+
+const AddProduct = () => {
   const { user } = useContext(AuthContext);
-  const [brand, setBrand] = useState("");
-  const handleSelectChange = (event) => {
-    setBrand(event.target.value);
+
+  const [postingDate, setPostingDate] = useState(new Date());
+  const [deadline, setDeadline] = useState(new Date());
+
+  const [jobCategory, setJobCategory] = useState("");
+  const handleCategoryChange = (event) => {
+    setJobCategory(event.target.value);
   };
 
-  const handleAddProduct = async (event) => {
+  const handleAddJob = async (event) => {
     event.preventDefault();
     const form = event.target;
-    const image = form.image.value;
-    const name = form.name.value;
-    const type = form.type.value;
-    const price = form.price.value;
+    const email = form.email.value;
+    const title = form.title.value;
+    const username = form.username.value;
+    const salleryStart = form.salleryStart.value;
+    const salleryEnd = form.salleryEnd.value;
+    const applicantNumber = form.applicantNumber.value;
     const description = form.description.value;
-    const rating = form.rating.value;
-    const product = {
-      name,
-      price,
-      type,
+    const jobObj = {
+      email,
+      title,
+      username,
+      jobCategory,
+      salleryStart,
+      salleryEnd,
       description,
-      rating,
-      image,
-      brand,
+      postingDate,
+      deadline,
+      applicantNumber,
     };
-    console.log(product);
+    console.log(jobObj);
     // try {
-    //   const response = await fetch(
-    //     "https://digital-store-server-940h97fnc-rabby-webs-projects.vercel.app/products",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify(product),
-    //     }
-    //   );
+    //   const response = await fetch("http://localhost:5000/jobs", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(jobObj),
+    //   });
     //   const result = await response.json();
-    //   console.log(result);
+
     //   if (result.acknowledged) {
-    //     Swal.fire("Product Add", "Successfully Product Add", "success");
+    //     // swal("done", "Job Posted Successfully", "success");
+    //     alert("success");
     //   }
     // } catch (error) {
     //   console.log(error);
     // }
-    // form.reset("");
+
+    form.reset("");
   };
 
   return (
-    <div className="bg-[#856bb350] rounded-md p-3 md:p-20 my-6 mx-2">
-      <h2 className="text-3xl font-bold text-center text-[#186AE3]">Add Job</h2>
-      <form onSubmit={handleAddProduct}>
-        <div className="md:flex">
-          <div className="form-control md:w-1/2 mx-2">
-            <label className="label ">
-              <span className="label-text text-lg text-white">Image</span>
-            </label>
-            <label className="">
-              <input
-                type="text"
-                name="image"
-                placeholder="Image URL"
-                className="input input-bordered w-full"
-              />
-            </label>
+    <div className="max-w-screen-xl px-4 mx-auto">
+      <div className="border shadow-xl rounded-md p-4 my-6">
+        <h2 className="text-2xl font-bold text-center text-[#152475]">
+          Add A Job
+        </h2>
+        <form onSubmit={handleAddJob}>
+          <div className="md:flex">
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label ">
+                <span className="label-text text-lg text-black">
+                  Picture URL for the Job Banner
+                </span>
+              </label>
+              <label className="">
+                <input
+                  defaultValue={user?.email}
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label">
+                <span className="label-text text-lg text-black">Job Title</span>
+              </label>
+              <label className="">
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Job Title"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
           </div>
-          <div className="form-control md:w-1/2 mx-2">
-            <label className="label">
-              <span className="label-text text-lg text-white">Name</span>
-            </label>
-            <label className="">
-              <input
-                type="text"
-                name="name"
-                placeholder="Product Name"
+          {/* ---- */}
+          <div className="md:flex">
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label">
+                <span className="label-text text-lg text-black">
+                  Logged In Username
+                </span>
+              </label>
+              <label className="">
+                <input
+                  defaultValue={user?.displayName}
+                  type="text"
+                  name="username"
+                  placeholder="Logged in user name"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label ">
+                <span className="label-text text-lg text-black ">
+                  Job Category
+                </span>
+              </label>
+              <select
                 className="input input-bordered w-full"
-              />
-            </label>
+                value={jobCategory}
+                onChange={handleCategoryChange}
+              >
+                <option selected>Choose Job Category</option>
+                <option value="On Site">On Site</option>
+                <option value="Remote">Remote</option>
+                <option value="Part Time">Part Time</option>
+                <option value="Hybrid">Hybrid</option>
+              </select>
+            </div>
           </div>
-        </div>
-        {/* ---- */}
-        <div className="md:flex">
-          <div className="form-control md:w-1/2 mx-2">
-            <label className="label">
-              <span className="label-text text-lg text-white">Type</span>
-            </label>
-            <label className="">
-              <input
-                type="text"
-                name="type"
-                placeholder="Product Type"
+          {/* ---- */}
+          <div className="md:flex">
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label ">
+                <span className="label-text text-lg text-black ">
+                  Sallery Range
+                </span>
+              </label>
+
+              <label className="flex items-center">
+                <input
+                  type="text"
+                  name="salleryStart"
+                  placeholder="000 $"
+                  className="input input-bordered w-full"
+                />
+                <span className="text-xl font-semibold mx-4">To</span>
+                <input
+                  type="text"
+                  name="salleryEnd"
+                  placeholder="000 $"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label ">
+                <span className="label-text text-lg text-black">
+                  Job Posting Date
+                </span>
+              </label>
+
+              <DatePicker
                 className="input input-bordered w-full"
+                selected={postingDate}
+                onChange={(date) => setPostingDate(date)}
               />
-            </label>
-          </div>
-          <div className="form-control md:w-1/2 mx-2">
-            <label className="label ">
-              <span className="label-text text-lg text-white">Price</span>
-            </label>
-            <label className="">
-              <input
-                type="text"
-                name="price"
-                placeholder="Product Price"
-                className="input input-bordered w-full"
-              />
-            </label>
-          </div>
-        </div>
-        {/* ---- */}
-        <div className="md:flex">
-          <div className="form-control md:w-1/2 mx-2">
-            <label className="label ">
-              <span className="label-text text-lg text-white">Brand Name</span>
-            </label>
-            <select
-              className="input input-bordered w-full"
-              value={brand}
-              onChange={handleSelectChange}
-            >
-              <option className="p-1 border" value="Samsung">
-                Samsung
-              </option>
-              <option className="p-1 border" value="Apple">
-                Apple
-              </option>
-              <option className="p-1 border" value="Microsoft">
-                Microsoft
-              </option>
-              <option className="p-1 border" value="Sony">
-                SONY
-              </option>
-              <option className="p-1 border" value="Lg">
-                LG
-              </option>
-              <option className="p-1 border" value="Toshiba">
-                Toshiba
-              </option>
-            </select>
+            </div>
           </div>
 
-          <div className="form-control md:w-1/2 mx-2">
-            <label className="label ">
-              <span className="label-text text-lg text-white">Rating</span>
+          <div className="md:flex">
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label ">
+                <span className="label-text text-lg text-black ">
+                  Application Deadline
+                </span>
+              </label>
+
+              <DatePicker
+                className="input input-bordered w-full"
+                selected={deadline}
+                onChange={(date) => setDeadline(date)}
+              />
+            </div>
+
+            <div className="form-control md:w-1/2 mx-2">
+              <label className="label ">
+                <span className="label-text text-lg text-black">
+                  Job Applicants Number
+                </span>
+              </label>
+              <label className="">
+                <input
+                  defaultValue={0}
+                  type="number"
+                  name="applicantNumber"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="form-control w-full mx-2 pr-4">
+            <label className="label">
+              <span className="label-text text-lg text-black">
+                Job Description
+              </span>
             </label>
             <label className="">
-              <input
-                type="number"
-                name="rating"
-                placeholder="Rating"
-                className="input input-bordered w-full"
-              />
+              <textarea
+                name="description"
+                className="textarea input input-bordered w-full"
+                placeholder="Job Description"
+              ></textarea>
             </label>
           </div>
-        </div>
 
-        <div className="form-control w-full mx-2 pr-4">
-          <label className="label">
-            <span className="label-text text-lg text-white">
-              Short description
-            </span>
-          </label>
-          <label className="">
+          <div className="text-center">
             <input
-              type="text"
-              name="description"
-              placeholder="Short Description"
-              className="input input-bordered w-full"
+              type="submit"
+              value="Add Job"
+              className="bg-[#152475] hover:bg-[#152475] text-white w-1/2 m-2 rounded-lg py-3 px-4 my-5 text-xl font-semibold"
             />
-          </label>
-        </div>
-
-        <div className="text-center">
-          <input
-            type="submit"
-            value="Add Job"
-            className="bg-[#186AE3] hover:bg-[#6C40B8] text-white w-1/2 m-2 rounded-lg py-3 px-4 my-5 text-xl font-semibold"
-          />
-        </div>
-      </form>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default AddJobs;
+export default AddProduct;

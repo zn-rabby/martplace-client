55fb75cc1e64de72b7be184ca3d83ae110ab02ea
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-06 21:04:32 +0600

    card UI add

--- src/page/Banner/Banner.jsx
+++ src/page/Banner/Banner.jsx
@@ -9,10 +9,11 @@ const Banner = () => {
               <h1 className="text-3xl font-bold text-white">
                 FIND THE JOB THAT FITS YOUR LIFE
               </h1>
-              <h2 className="text-3xl md:text-5xl font-bold inline-block p-2 rounded-md bg-[#1306098e] text-[#186AE3]">
-                We offer thousands of jobs vacancies right now
-              </h2>
-              <p className="text-white text-lg">Instant Job Finder</p>
+              <div className="bg-[#1006135e]">
+                <h2 className="text-3xl md:text-5xl font-bold inline-block p-2 rounded-md  bg-gradient-to-r from-primary to-[#EF4741] bg-clip-text text-transparent text-[#186AE3]">
+                  We offer thousands of jobs vacancies right now
+                </h2>
+              </div>
               <button className="bg-[#186AE3] text-white text-xl font-semibold py-3 px-5 rounded flex justify-center items-center mb-6 mx-auto md:text-block">
                 Explore More{" "}
                 <MdKeyboardDoubleArrowRight className="text-2xl font-bold"></MdKeyboardDoubleArrowRight>
--- src/page/JobsCarts/JobsCarts.jsx
+++ src/page/JobsCarts/JobsCarts.jsx
@@ -1,36 +1,56 @@
+import { BsFillCalendar2CheckFill } from "react-icons/bs";
+import { MdPriceChange } from "react-icons/md";
+
 const JobsCarts = ({ item }) => {
-  console.log(item);
+  const {
+    email,
+    title,
+    jobCategory,
+    salleryStart,
+    salleryEnd,
+    description,
+    deadline,
+  } = item || {};
   return (
-    <div>
-      <div className="relative flex flex-col text-[#F04842] bg-[#0d131f] border shadow-md w-96 rounded-xl bg-clip-border">
-        <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white  rounded-xl bg-clip-border">
-          <img
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
-            className="object-cover w-full h-64"
-          />
-        </div>
-        <div className="p-6">
-          <div className="flex items-center justify-between mb-2">
-            <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-              Apple AirPods
-            </p>
-            <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-              $95.00
-            </p>
-          </div>
-          <p className="block font-sans text-sm antialiased font-normal leading-normal text-white opacity-75">
-            With plenty of talk and listen time, voice-activated Siri access,
-            and an available wireless charging case.
-          </p>
-        </div>
-        <div className="p-6 pt-0">
-          <button
-            className="block w-full select-none rounded-lg bg-[#6C40B8] text-white py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
+    <div className="max-full gap-2 m-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h2>
+
+      <h4 className="text-xl font-semibold text-white flex gap-2 items-center">
+        Price range:
+        <span>${salleryStart}</span>
+        <span className="text-lg font-medium text-white">-</span>
+        <span>${salleryEnd}</span>
+      </h4>
+      <p className="mb-3 font-normal text-white">{description.slice(0, 100)}</p>
+
+      <div className="flex justify-between text-lg font-semibold">
+        <h2 className="flex gap-2 items-center text-[#6C40B8]">
+          <BsFillCalendar2CheckFill></BsFillCalendar2CheckFill>{" "}
+          {deadline.slice(0, 10)}
+        </h2>
+        <a
+          href="#"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#6C40B8] rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 "
+        >
+          Bid Now
+          <svg
+            className="w-3.5 h-3.5 ml-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
           >
-            Add to Cart
-          </button>
-        </div>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
       </div>
     </div>
   );

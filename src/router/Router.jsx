8b42af86d11589d14ede41e7219fa68f83e5ad74
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-09 12:51:25 +0600

    routes add protect

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -11,12 +11,6 @@ import BidRequests from "../page/BidRequests/BidRequests";
 import JobsDetails from "../page/JobsDetails/JobsDetails";
 import Update from "../page/Update/Update";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
-// import AddProduct from "../page/AddProduct/AddProduct";
-// import PrivateRoute from "../PrivateRoute/PrivateRoute";
-// import Brand from "../page/Brand/Brand";
-// import MyCart from "../page/MyCart/MyCart";
-// import Update from "../page/Update/Update";
-// import Details from "../page/Details/Details";
 
 const router = createBrowserRouter([
   {
@@ -30,19 +24,35 @@ const router = createBrowserRouter([
       },
       {
         path: "/addJob",
-        element: <AddJobs></AddJobs>,
+        element: (
+          <PrivateRoute>
+            <AddJobs></AddJobs>
+          </PrivateRoute>
+        ),
       },
       {
         path: "/jobs/:id",
-        element: <JobsDetails></JobsDetails>,
+        element: (
+          <PrivateRoute>
+            <JobsDetails></JobsDetails>
+          </PrivateRoute>
+        ),
       },
       {
         path: "/postedJobs",
-        element: <PostedJobs></PostedJobs>,
+        element: (
+          <PrivateRoute>
+            <PostedJobs></PostedJobs>
+          </PrivateRoute>
+        ),
       },
       {
         path: "/jobs/update/:id",
-        element: <Update></Update>,
+        element: (
+          <Update>
+            <Update></Update>
+          </Update>
+        ),
       },
       {
         path: "/bids",
@@ -51,54 +61,12 @@ const router = createBrowserRouter([
             <Bids></Bids>
           </PrivateRoute>
         ),
-        // loader: () => fetch("http://localhost:5000/bids"),
       },
       {
         path: "/bidRequests",
         element: <BidRequests></BidRequests>,
         loader: () => fetch("http://localhost:5000/bids"),
       },
-      // {
-      //   path: "/brand/:brand_name",
-      //   element: <Brand></Brand>,
-      //   loader: () => fetch("/brand.json"),
-      // },
-      // {
-      //   path: "/products/:id",
-      //   element: <Update></Update>,
-      //   loader: ({ params }) =>
-      //     fetch(
-      //       `https://digital-store-server-940h97fnc-rabby-webs-projects.vercel.app/products/${params.id}`
-      //     ),
-      // },
-      // {
-      //   path: "/details/:id",
-      //   element: (
-      //     <PrivateRoute>
-      //       <Details></Details>
-      //     </PrivateRoute>
-      //   ),
-      //   loader: ({ params }) =>
-      //     fetch(
-      //       `https://digital-store-server-940h97fnc-rabby-webs-projects.vercel.app/products/${params.id}`
-      //     ),
-      // },
-      // {
-      //   path: "/myCart",
-      //   element: (
-      //     <PrivateRoute>
-      //       <MyCart></MyCart>
-      //     </PrivateRoute>
-      //   ),
-      // },
-      // {
-      //   path: "/addProduct",
-      //   element: (
-      //     <PrivateRoute>
-      //       <AddProduct></AddProduct>
-      //     </PrivateRoute>
-      //   ),
-      // },
       {
         path: "/login",
         element: <Login></Login>,
